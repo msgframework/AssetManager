169bcc2fd56cd4e4a866e9a0e903aac6ef8a1beb
MSGroupFM <admin@msgru.com>
2022-03-23 00:01:59 +0300

    Refactor WebAssetItemAttachBehaviorInterface

--- AssetItem/CoreAssetItem.php
+++ AssetItem/CoreAssetItem.php
@@ -2,9 +2,6 @@
 
 namespace Msgframework\Lib\AssetManager\AssetItem;
 
-use RocketCMS\Lib\Document\Document;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Uri\Uri;
 use Msgframework\Lib\AssetManager\WebAssetAttachBehaviorInterface;
 use Msgframework\Lib\AssetManager\WebAssetItem;
 
@@ -13,27 +10,13 @@ use Msgframework\Lib\AssetManager\WebAssetItem;
  */
 class CoreAssetItem extends WebAssetItem implements WebAssetAttachBehaviorInterface
 {
-	/**
-	 * Method called when asset attached to the Document.
-	 * Useful for Asset to add a Script options.
-	 *
-	 * @param   Document  $doc  Active document
-	 *
-	 * @return void
-	 */
-	public function onAttachCallback(Document $doc)
+    /**
+     * Method called when asset attached to the Document.
+     * Useful for Asset to add a Script options.
+     *
+     * @return void
+     */
+	public function onAttachCallback()
 	{
-		// Add core and base uri paths so javascript scripts can use them.
-		$doc->addScriptOptions(
-			'system.paths',
-			[
-				'root' => Uri::root(true),
-				'rootFull' => Uri::root(),
-				'base' => Uri::base(true),
-				'baseFull' => Uri::base(),
-			]
-		);
-
-		HTMLHelper::_('form.csrf');
 	}
 }
--- WebAssetAttachBehaviorInterface.php
+++ WebAssetAttachBehaviorInterface.php
@@ -9,13 +9,11 @@ use RocketCMS\Lib\Document\Document;
  */
 interface WebAssetAttachBehaviorInterface
 {
-	/**
-	 * Method called when asset attached to the Document.
-	 * Useful for Asset to add a Script options.
-	 *
-	 * @param   Document  $doc  Active document
-	 *
-	 * @return void
-	 */
-	public function onAttachCallback(Document $doc);
+    /**
+     * Method called when asset attached to the Document.
+     * Useful for Asset to add a Script options.
+     *
+     * @return void
+     */
+	public function onAttachCallback();
 }
